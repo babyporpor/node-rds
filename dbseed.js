2bf63279abe13187f641de34c700ec94c8978921
babyporpor <vapple7379@gmail.com>
2024-02-17 16:01:44 +0700

    Update dbseed.js

--- dbseed.js
+++ dbseed.js
@@ -1,9 +1,9 @@
 import  mysql from 'mysql';
 
 const con = mysql.createConnection({
-    host: "houserentdb.cbc8mqgu6oox.us-east-1.rds.amazonaws.com",
+    host: "houserentdb01.cfcoci68c37p.ap-southeast-2.rds.amazonaws.com",
     user: "admin",
-    password: "0YZCnztnoKnOQjNwCOGn"
+    password: "babyporpor"
 });
 
 // connect to the MySQL database
